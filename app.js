b0895143112d7ec61247fc1fa76565e130d82c7a
vaishali202010 <pvaishali@gmail.com>
2024-08-27 12:13:46 +0530

    commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+// Create a server object
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello, VAISHALI from nodejs!</h1>\n');
+});
+
+// The server listens on port 3000
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
